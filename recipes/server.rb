1d53c8464250ad35c08ab49ced06eab3ba16deb9
TheGov18 <thegov18@gmail.com>
2018-03-13 13:45:16 +0000

    Template update

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,8 +2,8 @@ powershell_script 'install IIS' do
   code 'add-WindowsFeature Web-server'
 end
 
-file 'c:\inetpub\wwwroot\Default.htm' do
-content '<h1>Hello, World! and Mita</h1>'
+template 'c:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
